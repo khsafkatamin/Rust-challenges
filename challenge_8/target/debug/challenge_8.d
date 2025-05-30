1056e66747a7065b43c47173da164974aea09501
khsafkatamin <safkat.amin@rwth-aachen.de>
2025-05-30 13:11:15 +0200

    challenge 8 solution

---
+++ challenge_8/target/debug/challenge_8.d
@@ -0,0 +1,1 @@
+/workspace/challenge_8/target/debug/challenge_8: /workspace/challenge_8/src/main.rs
---
+++ challenge_8/target/debug/deps/challenge_8-c908b09aa413d182.d
@@ -0,0 +1,5 @@
+/workspace/challenge_8/target/debug/deps/challenge_8-c908b09aa413d182: src/main.rs
+
+/workspace/challenge_8/target/debug/deps/challenge_8-c908b09aa413d182.d: src/main.rs
+
+src/main.rs:
